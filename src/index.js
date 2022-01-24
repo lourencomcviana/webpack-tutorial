6887732c511aba35f259cdc57ef6e3a3bab44bc2
Lourenco <lmviana@latam.stefanini.com>
2022-01-24 14:31:16 -0300

    adicionado outro componente com css separado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import HelloWorldButton from './components/hello-world-button/hello-world-button';
-
+import Heading from "./components/heading/heading";
 const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+
 helloWorldButton.render();
+heading.render();
